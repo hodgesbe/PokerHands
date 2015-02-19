e6c64b4438c1949ad8b0c132d4de5fb1bc3bb7c2
Brett Hodges <bhodges87@gmail.com>
2015-02-19 15:33:37 -0500

    Initial Commit

---
+++ PokerHands/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PokerHands
+//
+//  Created by Brett Hodges on 2/19/15.
+//  Copyright (c) 2015 Brett Hodges. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
