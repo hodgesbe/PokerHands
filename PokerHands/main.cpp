eb3a6fccf556627dd11db0282c83ae5e48361b90
Brett Hodges <bhodges87@gmail.com>
2015-02-20 11:18:31 -0500

    Reads in file. Added function "dealHands".

--- PokerHands/main.cpp
+++ PokerHands/main.cpp
@@ -8,9 +8,40 @@
 
 #include <fstream>
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+char player1[5], player2[5];
+int ccount=0;
+
+void dealHands(string line){
+    for (int i=0; i<line.length(); ++i) {
+        cout<<line.at(i)<<endl;
+    }
+    cout<<endl;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    ifstream infile("hands.txt");
+    
+    if (!infile) {
+        cerr <<"Error Opening File"<<endl;
+        exit(1);
+    }
+    
+    
+    int count = 0;
+    string line;
+    
+    while (count<1000) {
+        count++;
+        getline(infile, line);
+        dealHands(line);
+    }
+    
+    infile.close();
+    
     return 0;
 }
