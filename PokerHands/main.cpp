f7a7e1d90cca29741dbe132c36de962dcc7b5ad3
Brett Hodges <bhodges87@gmail.com>
2015-02-22 21:44:18 -0500

    Added hasStaight, hasFlush, hasFullHouse.

--- PokerHands/main.cpp
+++ PokerHands/main.cpp
@@ -9,6 +9,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <list>
 
 using namespace std;
 
@@ -24,7 +25,23 @@ int highCard(string playerCards){
     int highCard=0;
     
     for (int i=0; i<playerCards.length(); i+=3) {
-        if (playerCards.at(i)=='T'&& 10>highCard) {
+        if (playerCards.at(i)=='2'&& 2>highCard) {
+            highCard=2;
+        }else if(playerCards.at(i)=='3'&& 3>highCard){
+            highCard=3;
+        }else if(playerCards.at(i)=='4'&& 4>highCard){
+            highCard=4;
+        }else if(playerCards.at(i)=='5'&& 5>highCard){
+            highCard=5;
+        }else if(playerCards.at(i)=='6'&& 6>highCard){
+            highCard=6;
+        }else if(playerCards.at(i)=='7'&& 7>highCard){
+            highCard=7;
+        }else if(playerCards.at(i)=='8'&& 8>highCard){
+            highCard=8;
+        }else if(playerCards.at(i)=='9'&& 9>highCard){
+            highCard=9;
+        }else if(playerCards.at(i)=='T'&& 10>highCard) {
             highCard=10;
         }else if(playerCards.at(i)=='J'&& 11>highCard){
             highCard=11;
@@ -34,8 +51,6 @@ int highCard(string playerCards){
             highCard=13;
         }else if(playerCards.at(i)=='A'&& 14>highCard){
             highCard=14;
-        }else if((int)playerCards.at(i)>highCard){
-            highCard=(int)playerCards.at(i);
         }
     }
     return highCard;
@@ -85,6 +100,104 @@ bool hasThree(string playerCards){
     return threeKind;
 }
 
+bool hasStraight (string playerCards){
+    list<int> cards;
+    bool isStraight = true;
+    int cardArr[5];
+    int count;
+    
+    for (int i = 0; i<playerCards.length(); i+=3) {
+        if(playerCards.at(i)=='A'){
+            cards.push_back(1);
+        }else if(playerCards.at(i)=='2'){
+            cards.push_back(2);
+        }else if(playerCards.at(i)=='3'){
+            cards.push_back(3);
+        }else if(playerCards.at(i)=='4'){
+            cards.push_back(4);
+        }else if(playerCards.at(i)=='5'){
+            cards.push_back(5);
+        }else if(playerCards.at(i)=='6'){
+            cards.push_back(6);
+        }else if(playerCards.at(i)=='7'){
+            cards.push_back(7);
+        }else if(playerCards.at(i)=='8'){
+            cards.push_back(8);
+        }else if(playerCards.at(i)=='9'){
+            cards.push_back(9);
+        }else if(playerCards.at(i)=='T'){
+            cards.push_back(10);
+        }else if(playerCards.at(i)=='J'){
+            cards.push_back(11);
+        }else if(playerCards.at(i)=='Q'){
+            cards.push_back(12);
+        }else if(playerCards.at(i)=='K'){
+            cards.push_back(13);
+        }
+    }
+    
+    cards.sort();
+    
+    for (list<int>::iterator it = cards.begin(); it != cards.end(); ++it) {
+        cardArr[count]=*it;
+        count++;
+    }
+    
+    while (isStraight) {
+        for (int i = 0; i<5; ++i) {
+            for (int j = 1; j<5; ++j) {
+                if (cardArr[i]+1 == cardArr[j]) {
+                    isStraight = true;
+                }else{
+                   isStraight = false;
+                }
+            }
+        }
+    }
+    
+    return isStraight;
+}
+
+bool hasFlush(string playerCards){
+    bool isFlush = true;
+    while (isFlush) {
+        for (int i = 1; i<playerCards.length(); i+=3) {
+            for (int j = 2; j<playerCards.length(); j+=3) {
+                if (playerCards[i]==playerCards[j]) {
+                    isFlush = true;
+                }else{
+                    isFlush = false;
+                }
+            }
+        }
+    }
+    return isFlush;
+}
+
+bool hasFullHouse(string playerCards){
+    if (hasThree(playerCards)&&has2Pair(playerCards)) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool hasFour(string playerCards){
+
+    return true;
+}
+
+bool hasStraightFlush(string playerCards){
+    
+    return true;
+}
+
+bool hasRoyalFlush(string playerCards){
+    
+    return true;
+}
+
 //Main
 int main(int argc, const char * argv[]) {
     
@@ -107,7 +220,11 @@ int main(int argc, const char * argv[]) {
         cout<<"Player 2's hand: "<<player2<<endl;
         //cout<<"Player 1 has Pair? " <<hasPair(player1)<<endl;
         //cout<<"Player 1 has 2Pair? " <<has2Pair(player1)<<endl;
-        cout<<"Player 2 has Three of a kind: "<<hasThree(player2)<<endl;
+        //cout<<"Player 2 has Three of a kind: "<<hasThree(player2)<<endl;
+        //hasStraight(player2);
+        //cout<<"Player 2's high card is: "<<highCard(player2)<<endl;
+        cout<<"Has Straight?: "<<hasStraight(player2)<<endl;
+        
     }
     
     infile.close();
